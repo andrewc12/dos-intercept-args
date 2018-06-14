4e83dbe701b97e1e0181ce0ec41bd6c24de080cf
Andrew Innes <andrew.c12@gmail.com>
2018-06-14 17:45:35 +0800

    Clean up

--- arg.c
+++ arg.c
@@ -9,49 +9,33 @@ void main(int argc, char *argv[])    /* command line Arguments */
 {
     int i;
     int len;
+    int totallen = 0;
     char str[1024];
-    for (i = 0;i < argc;i++)
-    {
-        printf("%s ", argv[i]);        /* Printing the string */
-        len = strlen(argv[i]);
-        printf("Length of |%s| is |%d|\n", argv[i], len);
-    }    
-
-
+    
     printf("Number of argc %d \n", argc);
+    for (i = 0;i < argc;i++)
+    {  
+        len = strlen(argv[i]);
+        printf("Length of |%s| is |%d|\n", argv[i], len); /* Printing the string */
+        totallen = totallen + strlen(argv[i]); /*  get  total length of string. use to see if bigger than buffer */
+    }
+    totallen = totallen + 1;
+    printf("total Length is |%d|\n", totallen);
+
+
+
     for (i = strlen(argv[0]);i >= 0;i--)
     {
-	/*printf("%d \n", i);*/
 	if (argv[0][i] == '.'){
 	    i--;
-	    /*printf("%s %d \n", argv[i], i);        /* Printing the string */
 	    argv[0][i] = '_'; /* Change last char before .extension to _ */
 	    break;
 	}
     }    
     
-    
-    
-    for (i = 0;i < argc;i++)
-    {
-        printf("%s ", argv[i]);        /* Printing the string */
-        len = strlen(argv[i]);
-        printf("Length of |%s| is |%d|\n", argv[i], len);
-    }        
-    
-    
-    /*  get  total length of string. use to see if bigger than buffer */
-    len = 0;
-    for (i = 0;i < argc;i++)
-    {
-        len = len + strlen(argv[i]);
-        printf("total Length is |%d|\n", len);
-    }  
-    len = len + 1;
-    
     /* https://stackoverflow.com/questions/308695/how-do-i-concatenate-const-literal-strings-in-c */
 
-    if (len <= 1024){
+    if (len <= 1024){ /* check to see if it will fit then concatenate argv*/
 	    strcpy(str, argv[0]);
         for (i = 1;i < argc;i++)
         {
