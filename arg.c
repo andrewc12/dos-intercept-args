05304626cdc4cf67fb54b2045e6ad7b16a89144c
Andrew Innes <andrew.c12@gmail.com>
2018-06-14 17:22:14 +0800

    Print number of args and try string manipulation

--- arg.c
+++ arg.c
@@ -9,8 +9,29 @@ void main(int argc, char *argv[])    /* command line Arguments */
     for (i = 0;i < argc;i++)
     {
         printf("%s ", argv[i]);        /* Printing the string */
-        printf("\n");
     }
+    printf("\n");
     printf("Wait for 5 seconds to exit.\n");
     sleep(5);
+
+    printf("argc %d \n", argc);
+    for (i = argc;i >= 0;i--)
+    {
+	printf("%d \n", i);
+	if (argv[i] == '.'){
+	    i--;
+	    printf("%s %d \n", argv[i], i);        /* Printing the string */
+	    argv[i] = '_'; /* Change last char before .extension to _ */
+	    break;
+	}
+    }
+
+    for (i = 0;i < argc;i++)
+    {
+	printf("%s ", argv[i]);        /* Printing the string */
+    }
+    printf("\n");
+    printf("Wait for 5 seconds to exit.\n");
+    sleep(5);
+
 }
